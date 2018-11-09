95ef5842dd597f03d4e22e60469d64eca1f82478
dlike230 <dlike230@gmail.com>
2018-11-09 18:26:32 -0500

    Added some pure react test schedblocks

--- public/js/schedule_components.js
+++ public/js/schedule_components.js
@@ -37,10 +37,15 @@ var SchedGrid = function (_React$Component) {
             for (var i = 0; i < this.state.schedLines.length; i++) {
                 lines.push(React.createElement(SchedLine, { key: i, y: this.state.schedLines[i] }));
             }
+            var blocks = [];
+            for (var _i = 0; _i < 10; _i++) {
+                blocks.push(React.createElement(SchedBlock, { topC: "green", id: _i, assignedClass: "x", letterDay: "M", key: _i, y: _i * 10, x: 50, width: 10, height: 10, name: "test" }));
+            }
             console.log(lines);
             return React.createElement(
                 "div",
                 { id: "SchedGrid" },
+                blocks,
                 lines
             );
         }
@@ -85,19 +90,55 @@ var SchedBlock = function (_React$Component3) {
         var _this3 = _possibleConstructorReturn(this, (SchedBlock.__proto__ || Object.getPrototypeOf(SchedBlock)).call(this, props));
 
         _this3.y = props.y;
-        _this3.x = prop.x;
+        _this3.x = props.x;
         _this3.width = props.width;
         _this3.height = props.height;
         _this3.letterDay = props.letterDay;
         _this3.topC = props.topC;
+        _this3.id = props.id;
+        _this3.assignedClass = props.assignedClass;
+        _this3.name = props.name;
         return _this3;
     }
 
     _createClass(SchedBlock, [{
         key: "render",
         value: function render() {
-            return React.createElement("div", { className: "SchedBlock_container " + this.letterDay + " " + this.topC,
-                style: { left: this.x + "%", top: this.y + "%", width: this.width + "%", height: this.height + "%" } });
+            return React.createElement(
+                "div",
+                { className: "SchedBlock_container " + this.letterDay + " " + this.topC,
+                    style: { left: this.x + "%", top: this.y + "%", width: this.width + "%", height: this.height + "%" } },
+                React.createElement(
+                    "div",
+                    { className: "SchedBlock " + this.letterday + " " + this.topC + " " + this.assignedClass, id: this.id,
+                        onClick: "angular.element(document.body).scope().clearSearch();" + "angular.element(document.body).scope().initiateSearch(" + this.assignedClass + ", " + "'courseIDSearch');" },
+                    React.createElement(
+                        "div",
+                        { className: "CloseX", style: { width: 100 + "%", height: 100 + "%" } },
+                        React.createElement(
+                            "span",
+                            {
+                                onClick: "e.stopPropagation(); angular.element(document.body).scope().sched.AddRem(thisBlock.class);" },
+                            "X"
+                        )
+                    ),
+                    React.createElement(
+                        "div",
+                        { className: "NeedAssc",
+                            title: "Registration is required for an associated section." },
+                        React.createElement(
+                            "b",
+                            null,
+                            "!"
+                        )
+                    ),
+                    React.createElement(
+                        "span",
+                        { className: "SecName" },
+                        this.name
+                    )
+                )
+            );
         }
     }]);
 
--- react_components/schedule_components.js
+++ react_components/schedule_components.js
@@ -19,8 +19,12 @@ class SchedGrid extends React.Component{
         for(let i = 0; i < this.state.schedLines.length; i++){
             lines.push(<SchedLine key = {i} y = {this.state.schedLines[i]}/>);
         }
+        let blocks = [];
+        for(let i = 0; i < 10; i++){
+            blocks.push(<SchedBlock topC={"green"} id ={i} assignedClass={"x"} letterDay={"M"} key = {i} y={i*10} x={50} width ={10} height = {10} name = {"test"}/>);
+        }
         console.log(lines);
-        return <div id = {"SchedGrid"}>{lines}</div>;
+        return <div id = {"SchedGrid"}>{blocks}{lines}</div>;
     }
 }
 
@@ -46,7 +50,7 @@ class SchedBlock extends React.Component{
     constructor(props){
         super(props);
         this.y = props.y;
-        this.x = prop.x;
+        this.x = props.x;
         this.width = props.width;
         this.height = props.height;
         this.letterDay = props.letterDay;
@@ -60,13 +64,13 @@ class SchedBlock extends React.Component{
         return<div className = {"SchedBlock_container "+this.letterDay+" "+this.topC}
                     style = {{left:this.x+"%",top:this.y+"%",width:this.width+"%",height:this.height+"%"}}>
                     <div className={"SchedBlock " +this.letterday+" "+ this.topC+" "+this.assignedClass} id = {this.id}
-                         onclick={"angular.element(document.body).scope().clearSearch();"+
+                         onClick={"angular.element(document.body).scope().clearSearch();"+
                                   "angular.element(document.body).scope().initiateSearch("+this.assignedClass+", "+"'courseIDSearch');"}>
-                        <div className="CloseX" style="width:100%;height:100%;"><span
-                            onclick="e.stopPropagation(); angular.element(document.body).scope().sched.AddRem(thisBlock.class);">X</span></div>
-                        <div className="NeedAssc"
-                             title="Registration is required for an associated section."><b>!</b></div>
-                        <span className="SecName">{this.name}</span>
+                        <div className={"CloseX"} style={{width:100+"%",height:100+"%"}}><span
+                            onClick={"e.stopPropagation(); angular.element(document.body).scope().sched.AddRem(thisBlock.class);"}>X</span></div>
+                        <div className={"NeedAssc"}
+                             title={"Registration is required for an associated section."}><b>!</b></div>
+                        <span className={"SecName"}>{this.name}</span>
                     </div>
                 </div>
     }
